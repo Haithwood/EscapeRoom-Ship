6800d10db4eb8fe61bf812a671a894179efc02f7
ANASTASIA ZAIKINA <az288756@falmouth.ac.uk>
2026-06-26 13:42:01 +0100

    Added: LED test script

---
+++ Code/Tests/LED-TEST/LED-TEST.ino
@@ -0,0 +1,50 @@
+/* 
+SHIP LED TEST CODE
+
+*/
+
+#include <Arduino.h>
+#include <FastLED.h>
+
+#define NUM_LEDS 8
+#define DATA_PIN 39
+
+CRGB leds[NUM_LEDS];
+
+void setup() {
+  // Cannot have serial, otherwise pin 1 stays lit
+  FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
+  FastLED.clear();
+  FastLED.show();
+
+  for (int i = 1; i < 32; i++) {
+    pinMode(i, OUTPUT);
+  }
+}
+
+void loop() {
+  // order for the Compass LEDs to flash
+  int order[] = { 2, 3, 4, 5, 6, 7, 0, 1, 2 };
+
+  // Flashing compass lights
+  for (int i = 0; i < 9; i++) {
+    leds[order[i]] = CRGB::Blue;
+    FastLED.show();
+    delay(200);
+    FastLED.clear();
+    FastLED.show();
+    delay(200);
+  }
+
+  // light up map path
+  for (int i = 1; i < 32; i++) {
+    digitalWrite(i, HIGH);
+    delay(50);
+  }
+  delay(1000);
+  // clear map path
+  for (int i = 1; i < 32; i++) {
+    digitalWrite(i, LOW);
+    delay(50);
+  }
+}
